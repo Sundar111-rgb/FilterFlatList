62ed4429bcd5e4078b207dfc197979f95dc83ba3
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2020-10-06 19:02:36 +0530

    6 Oct 7:02 Updation

--- Washing.js
+++ Washing.js
@@ -1,8 +1,9 @@
 
 import React, { Component } from 'react';
-import { View, TextInput, Text, TouchableOpacity, Alert, FlatList } from 'react-native';
+import { View, TextInput, Text, TouchableOpacity, Alert, FlatList, } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
+import CheckBox from '@react-native-community/checkbox';
 
 export default class Washing extends Component {
 
@@ -11,18 +12,79 @@ export default class Washing extends Component {
 
         this.state = {
             cost: 0,
-            toggle: true,
+            toggle: false,
             costprice:true,
             quantity:0,
             data: [],
-             
+            lists:[],
             
-        }
+          //  isChecked : [],
+            selectedLists:[],
+            isChecked : false
+         }
     }
 
+
+       
        componentDidMount() {
+
+        let initialCheck = this.state.lists.map(() => false);
+    //   console.log(initialCheck);
+        this.setState({isChecked : initialCheck})
            this.ApiFetch()
        }
+
+     
+       isIconCheckedOrNot = (item,index) => {
+
+        // if(this.state.isChecked){
+        //     alert('h')
+        //     this.setState({isChecked:item})
+        // }else {
+        //     alert('Y')
+        //     this.setState({isChecked:item})
+        // }
+      // this.state.isChecked[index] = !this.state.isChecked[index];
+       console.log('--------------------',item.title)
+        let  isChecked = this.state.isChecked
+        let selectedLists = this.state.selectedLists
+        isChecked[index] = !isChecked[index];
+        this.setState({ isChecked : isChecked});
+        if(isChecked[index] == true) {
+            selectedLists.push(item.price)
+        } else {            
+            selectedLists.pop(item.price)
+        }
+
+        alert(selectedLists)
+       }
+    
+
+       ApiFetch1 = () => {
+        var formdata = new FormData()
+       formdata.append('name','sundar')
+        formdata.append('email','')
+        formdata.append('mobile',)
+        formdata.append('password','chauhan1234@#')
+        formdata.append('device_type','android')
+        formdata.append('device_token','abcd')
+
+    var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+      };
+  
+ fetch("https://www.webmobril.org/dev/dogtraining/api/v1/register-user", requestOptions)
+    .then(response => response.json())
+    .then((response) => {    
+      console.log("inspection response=-=+++++=->>",response) 
+
+     // this.setState({data:response.result})
+      
+    })
+    .catch(error => console.log('error', error));
+      }
 
     ApiFetch = () => {
             var formdata = new FormData()
@@ -46,9 +108,18 @@ export default class Washing extends Component {
           }
     
 
-    call = () => {
-        this.setState({ toggle: !this.state.toggle })
+    Call = (index) => {
+      //  this.setState({toggle : !this.state.toggle})
+        this.setState({ isChecked : !this.state.isChecked[index] })
     }
+
+    onCheck = (item, index) => {
+        let items; 
+     //   item.data[index].checked = item.data[i].checked ? ! item.data[i].checked : true
+         items[index] = item.price
+       this.setState({list:items})
+        alert(list)
+     }
 
     adddd = (item) => {
       
@@ -66,7 +137,7 @@ export default class Washing extends Component {
 
     renderItem = (item,index) => {
         return(
-            <View style={{ width: wp('90%'), height: hp('45%'), alignSelf: 'center', elevation: 10, marginTop: hp('1%'), backgroundColor: '#fff' }}>
+            <View style={{ width: wp('90%'), height: 'auto', alignSelf: 'center', elevation: 10, marginTop: hp('1%'), backgroundColor: '#fff',paddingBottom:hp('2%') }}>
             <View style={{ width: wp('90%'), height: hp('3.5%'), flexDirection: 'row', backgroundColor: 'lightgrey', elevation: 0, marginBottom: hp('1%') }}>
             <View style={{ width: wp('50%'), paddingStart: 17, paddingTop: 3 }}><Text style={{ alignSelf: 'flex-start', color: '#000', fontWeight: 'bold' }}>{item.name}</Text></View>
             <View style={{ width: wp('20%'), paddingTop: 3 }}><Text style={{ alignSelf: 'center', color: '#000', fontWeight: 'bold' }}>Quantity</Text></View>
@@ -85,27 +156,27 @@ export default class Washing extends Component {
         return(
             <View style={{ width: wp('90%'), height: hp('3%'), flexDirection: 'row', borderWidth: 1, borderColor: '#000', marginTop: hp('1%') }}>
                
-        <Text>{item.title}</Text>
+        <Text>{item.price}</Text>
         </View>
         )
     }
+
+   
 
     renderItemChild = (item,index) => {
         return(
             <View style={{ width: wp('90%'), height: hp('3%'), flexDirection: 'row', borderWidth: 0, borderColor: '#000', marginTop: hp('1%') }}>
                
             <View style={{flex:1.3, flexDirection: 'row', width: wp('60%'), borderWidth: 0,borderRadius:1, borderColor: '#000', alignItems: 'center', paddingStart: hp('1%') }}>
-                <TouchableOpacity onPress={() => this.call()}>
-                    {this.state.toggle ? <Icons name='checkbox-blank-outline' size={18} /> : <Icons name='check-box-outline' size={18} />}
-                </TouchableOpacity>
+                <CheckBox value={this.state.isChecked[index]}  onValueChange={() => this.isIconCheckedOrNot(item,index)}  />
                 <Text style={{ alignSelf: 'center', fontSize: hp('1.4%'), paddingStart: 0,fontWeight:'bold' }}>{item.title}</Text>
             </View>
 
             <View style={{flex:0.4,
                 flexDirection: 'row', borderColor: '#000',
-                marginLeft: hp('1.7%'), borderWidth: 0, borderColor: '#fff',
+                marginLeft: hp('1.7%'), borderWidth: 1, borderColor: '#000',
                 width: wp('20%'), height: hp('4%'), alignSelf: 'center',
-                elevation: 0, marginTop: hp('0%'), justifyContent: 'center',
+                elevation: 0, marginLeft: hp('2%'), justifyContent: 'center',
                 backgroundColor: 'light'
             }}>
                 <View style={{flexDirection:'row',marginTop:hp('0.5%')}}>
@@ -154,15 +225,13 @@ export default class Washing extends Component {
 
                
                 <View style={{ flexDirection: 'row', width: wp('65%'), borderWidth: 0, borderColor: '#000', alignItems: 'center', paddingStart: hp('4%'), marginTop: hp('1%') }}>
-                    <TouchableOpacity onPress={() => this.call()}>
-                        {this.state.toggle ? <Icons name='checkbox-blank-outline' size={18} /> : <Icons name='check-box-outline' size={18} />}
-                    </TouchableOpacity>
+                <CheckBox  />
+
                     <Text style={{ alignSelf: 'center', fontSize: 15, paddingStart: 2 }}>Dry Cleaning</Text>
                 </View>
                 <View style={{ flexDirection: 'row', width: wp('45%'), borderWidth: 0, borderColor: '#000', alignItems: 'center', paddingStart: hp('4%'), marginTop: hp('1%') }}>
-                    <TouchableOpacity onPress={() => this.call()}>
-                        {this.state.toggle ? <Icons name='checkbox-blank-outline' size={18} /> : <Icons name='check-box-outline' size={18} />}
-                    </TouchableOpacity>
+                <CheckBox />
+
                     <Text style={{ alignSelf: 'center', fontSize: 15, paddingStart: 2 }}>Press Only</Text>
                 </View>
 
@@ -170,7 +239,7 @@ export default class Washing extends Component {
                     <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Total Amount : ${this.state.cost}</Text>
                 </View>
 
-                <TouchableOpacity onPress={() => alert('Hello')}>
+                <TouchableOpacity onPress={() => alert(this.state.list)}>
                     <View style={{ width: wp('80%'), elevation: 20, height: hp('6%'), backgroundColor: '#48cfc9', borderRadius: 30, alignSelf: 'center', justifyContent: 'center', marginTop: hp('0%') }}>
                         <Text style={{ color: '#fff', alignSelf: 'center', fontSize: 20, fontWeight: 'bold' }}>Confirm</Text>
                     </View>
