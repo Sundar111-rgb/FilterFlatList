0db8c0d4597a92ae018ae84cd09428d70ed57b51
SundarKumarChauhan <67180616+Sundar111-rgb@users.noreply.github.com>
2021-01-18 23:31:09 +0530

    Add files via upload
    Multiple Reducers

---
+++ App.js
@@ -0,0 +1,70 @@
+
+import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+
+function App() {
+    const roll = useSelector(state => state.roll);
+    const name = useSelector(state => state.name);
+    const address = useSelector((state) => {
+        return state.address
+    })
+
+    const empid = useSelector(state => state.empid);
+    const empName = useSelector(state => state.empName);
+    const empAddress = useSelector((state) => {
+        return state.empAddress
+    })
+
+    const dispatch = useDispatch();
+    return (
+        <div className="App">
+            <h1>{roll}</h1>
+            <h1>{name}</h1>
+            <h1>{address}</h1>
+            <hr/>
+                <h1>{empid}</h1>
+                <h1>{empName}</h1>
+                <h1>{empAddress}</h1>
+            <button onClick={() => dispatch({
+                type: 'CHANGE_NAME',
+                payload: { roll: 1,
+                           name: 'Chauhan',
+                           address: 'Mumbai'
+                          }
+            })}>Click
+            </button>
+
+            <button onClick={() => dispatch({
+                type: 'CHANGE_EMP',
+                payload: { empid: 1,
+                           empName: 'Chauhan',
+                           empAddress: 'Mumbai'
+                         }
+            })}>Click
+            </button>
+        </div>
+    );
+}
+
+    //  changeName : (name) => {
+    //     const dispatch = useDispatch();
+    //     dispatch({type: 'CHANGE_NAME', payload: name})
+    // }
+
+// const mapStateToProps = (state) => {
+//   return{
+//     myname : state.name
+//   }
+// }
+//
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//   changeName : (name) => {
+//     dispatch({type: 'CHANGE_NAME', payload: name})
+//   }
+// }
+// }
+//
+// export default connect(mapStateToProps,mapDispatchToProps)(App);
+
+export default  App;
